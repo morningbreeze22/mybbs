e14971ff68491f641a2ac9543183d99d95974abc
jim <jim.song@icitymobile.com>
2019-07-19 10:28:18 +0800

    add configs in producition.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  #邮件验证
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => "https://immense-headland-99459.herokuapp.com"  }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default :charset => "utf-8"
+
+
+
 end
