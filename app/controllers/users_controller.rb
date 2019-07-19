142c273d5993b4b766022b52f848604afbe20e53
jim <jim.song@icitymobile.com>
2019-07-19 10:14:28 +0800

    修改migrate文件

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  load_and_authorize_resource
+  #before_action :authenticate_user!    #检查权限
+  before_action :authenticate_user!
+  def index
+    @users=User.all
+  end
+
+  def new
+
+  end
+
+  def create
+    @user= User.new(user_params)
+    @user.save
+    redirect_to users_path
+  end
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def destroy
+    @user=User.find_by_id(params[:id])
+    @user.destroy
+    redirect_to users_path
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:email, :username, :password, :role)
+    end
+
+end
